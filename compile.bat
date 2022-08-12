465b54423d1a13d3b8095f9bbde23122f01d776c
bravexample <106252820+bravexample@users.noreply.github.com>
2022-08-12 15:45:51 +0800

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 @echo off
-cl /O2 /favor:blend /Iinclude /Fe:release.exe source\glad.c main.c /link /NODEFAULTLIB:MSVCRT lib\glfw3dll.lib OpenGL32.lib
+cl /O2 /favor:blend /std:c17 /Iinclude /Fe:release.exe source\glad.c main.c /link /NODEFAULTLIB:MSVCRT lib\glfw3dll.lib OpenGL32.lib
 del *obj
